6293c45f0c87488104439d4e4ba129aeaf6b753b
anqiwan <anqiwan@gmail.com>
2017-10-27 18:18:12 -0400

    final

--- main.js
+++ main.js
@@ -12,45 +12,40 @@ var colors = ["Strawberry", "Night Moon", "Camouflage", "Crazyberry", "Blackberr
 var imgs = ['collars/red.png', 'collars/yellow.png', 'collars/camo.png','collars/gradient.png','collars/black.png', 'collars/orange.png'];
 var sizedescriptions = ["Tiny (less than 15 lb)", "Small (15-30 lb)", "Medium (30-50 lb)", "Large (50-70 lb)"];
 var sizes = ["Tiny", "Small", "Medium", "Large"];
-
-
-var cartitems = 0;
+var cartitems = 0; // keeps quantities of cart
 
 $(document).ready(function() {
-
    //initiate a new item to be stored.
-   //var newItem;
    var newItem = new Item();
-   //initiate new stored item array
+   //initiate new stored item arrays
    var storedItems;
+   var wishlist;
 
    //'stored' is the array that contains the stored items
    storedItems = JSON.parse(localStorage.getItem("stored"));
-
-   //grab the array and see
-   console.log(storedItems);
+   wishlist = JSON.parse(localStorage.getItem("wish"));
 
    if (storedItems === null) {
-      console.log("null"); // should only be first time, initiate new array
+      // should only be first time, initiate new array
       storedItems = [];  
     } 
 
-    //capture color
+   if (wishlist === null) {
+      wishlist = [];  
+    } 
+
+    //capture color swatch
     $('.swatch').click(function() {
       //initiate a new item if they click on a new swatch.
-      
       var color = $(this).attr('id');
       //grab index of the color + change description text
       var index =  ($(this).index());
       var displaytext = colors[index];
       $("#color").text("Color: " + displaytext);
-      //change image of dispslay
+      //change image of display
       $('#collarpic').attr('src', imgs[index]);
-
       newItem.color = index;
       newItem.photo = imgs[index];
-
-      console.log(newItem.color);
     });
 
    //capture size 
@@ -65,25 +60,33 @@ $(document).ready(function() {
       var displaytext = sizedescriptions[index];
       $("#size").text("Size: " + displaytext);
       newItem.size = index;
-      console.log(newItem.size);
     });
 
 
    //function to update cart when add button is clicked
    $('#add').click(function(){
-    console.log(newItem);
-    $("#add").text("Added to Cart!");
-    cartitems += 1;
-    $("#cart").text("Cart" + " (" + cartitems.toString() + ") " );
-    storedItems.push(newItem);
-    localStorage.setItem("stored", JSON.stringify(storedItems));
-    console.log( storedItems);
+     //console.log(newItem);
+     $("#add").text("Added to Cart!");
+     cartitems += 1;
+     $("#cart").text("Cart" + " (" + cartitems.toString() + ") " );
+     storedItems.push(newItem);
+     localStorage.setItem("stored", JSON.stringify(storedItems));
 
-    //create a new item and clear the old one.
-    newItem = new Item();
+     //clear the item 
+     newItem = new Item();
   });
 
+   //add something to wishlist
+   $('#addtowishlist').click(function(){
+     $("#addtowishlist").text("Added to list!");
+     $("#cart").text("Cart" + " (" + cartitems.toString() + ") " );
+     wishlist.push(newItem);
+     localStorage.setItem("wish", JSON.stringify(wishlist));
+     //create a new item and clear the old one.
+     newItem = new Item();
+  });
 
+   //update cart status
    var cartitems = storedItems.length;
    if (cartitems === 0) {
       $("#cartstatus").text("You have no items in your shopping cart.");
@@ -91,19 +94,19 @@ $(document).ready(function() {
    $("#cart").text("Cart" + " (" + cartitems.toString() + ") " );
 
   
-
     //populate list with stored items
     var listItems = JSON.parse(localStorage.getItem("stored"));
 
-    /////iterate through the array and create a new entry for each cart item////
+    //iterate through the array and create a new entry for each CART item
     for (var i = 0; i < listItems.length; i++) {
+
       imgsource = listItems[i].photo;
       var elem = $("<div>", { "class": "element"});
       var size = listItems[i].size;
       $("#itemlist").append(elem);
 
+      //append the image of the element
       var details = $("<div>", { "class": "details"});
-
       $(elem).append('<img class = "thumb" src=' + imgsource +' />');
 
       // append a div with type, size, color
@@ -125,35 +128,69 @@ $(document).ready(function() {
       var removebutton = $("<button>", { "class": "delete"});
       removebutton.append ("Remove Item");
       $(remove).append(removebutton);
-      console.log (size);
 
     }
 
+
+    //populate list with stored items
+    var wishlistItems = JSON.parse(localStorage.getItem("wish"));
+
+    /////iterate through the array and create a new entry for each WISHLIST item
+    for (var i = 0; i < wishlistItems.length; i++) {
+      imgsource = wishlistItems[i].photo;
+      var elem = $("<div>", { "class": "element"});
+      var size = wishlistItems[i].size;
+      $("#wishlistlist").append(elem);
+
+      //add product image
+      var details = $("<div>", { "class": "details"});
+      $(elem).append('<img class = "thumb" src=' + imgsource +' />');
+
+      // append a div with type, size, color
+      $(elem).append(details);
+      $(details).append("<h3> GPS Tracking Collar </h3>");
+      $(details).append("<h3> Size: "+ sizes[wishlistItems[i].size] +" </h3>");
+      $(details).append("<h3> Color: "+ colors[wishlistItems[i].color] +" </h3>");
+
+      // append a div with price
+      var price = $("<div>", { "class": "price"})
+      $(elem).append(price);
+      price.append("<h3> $75 </h3>");
+
+      // append a div with the remove button
+      var remove = $("<div>", { "class": "removesection"})
+      $(elem).append(remove);
+    }
+
+
     //when remove a product button is clicked
     $('.delete').click(function() {
-        //"item" is the container element for all product details
-        var item = this.parentNode.parentNode;
-        console.log( $(item).index() );
+      //"item" is div of the product entry
+      var item = this.parentNode.parentNode;
 
-        //find index of selected item and remove it from the locally stored array
-        var index = $(item).index();
-        listItems.splice(index,1);
-        localStorage.setItem("stored" , JSON.stringify(listItems));
+      //find index of selected item and remove it from the localstorage array
+      var index = $(item).index();
+      listItems.splice(index,1);
+      localStorage.setItem("stored" , JSON.stringify(listItems));
 
-        //remove the row from the cart and update cart quantity
-        item.parentNode.removeChild(item);
-        cartitems -= 1;
-        $("#cart").text("Cart" + " (" + cartitems.toString() + ") " );
+      //remove the row from the cart and update cart quantity
+      item.parentNode.removeChild(item);
+      cartitems -= 1;
+      $("#cart").text("Cart" + " (" + cartitems.toString() + ") " );
+
+      if (cartitems === 0) {
+        $("#cartstatus").text("You have no items in your shopping cart.");
+      }
     });
 
-     $("#clear").click(function() {
-    localStorage.setItem("stored" , JSON.stringify([]));
-      // makes it cleared
+    //clear all the items in cart
+    $("#clear").click(function() {
+      localStorage.setItem("stored" , JSON.stringify([]));
+      // clears all the items in cart.
       $("#button-action-text").text("Cleared!");
       $("#button-action-text").css("display", "block");
       $("#cart").text("Cart" + " (0)");
       $("#itemlist").empty();
     });
-
 
   });
